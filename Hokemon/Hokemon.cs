517f6ced4370d4059149d2b8c88e575e6a75c08d
OwedN <69062835+OwedN@users.noreply.github.com>
2021-01-14 21:44:34 +0000

    Add project files.

---
+++ Hokemon/Hokemon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon
+{
+    class Hokemon
+    {
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defence;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value;
+            }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value;
+            }
+        }
+
+        public void provide_name()
+        {
+            Console.WriteLine("What is the name of your Hokemon? :");
+            name = Console.ReadLine();
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("\n\n**********************\nName: {0}\nHealth: {1}/{2}\nAttack: {3}\nSpeed: {4}\nDefence: {5}", name, health, max_health, attack, speed, defence);
+        }
+
+        public int random_num_gen(int min, int max)
+        {
+            Random random = new Random();
+            int random_value;
+            random_value = random.Next(min, max);
+
+            return random_value;
+        }
+
+        public Hokemon()
+        {
+            provide_name();
+            max_health = 100;
+            health = random_num_gen(10, max_health);
+            attack = random_num_gen(10, 100);
+            speed = random_num_gen(10, 100);
+            defence = random_num_gen(10, 100);
+
+        }
+    }
+}
---
+++ Hokemon/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the world of Hokemon!!!\n\n");
+            Hokemon hoke1 = new Hokemon();
+
+            hoke1.get_details();
+        }
+    }
+}
